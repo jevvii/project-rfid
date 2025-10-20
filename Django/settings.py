fdfdb776b0aeba55336bc772710093e5551bdff3
jevvii <jayveemarcelo09@gmail.com>
2025-10-21 06:39:47 +0800

    Settings: database props

--- Django/settings.py
+++ Django/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = "Django.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rfid_with_sms_system',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
